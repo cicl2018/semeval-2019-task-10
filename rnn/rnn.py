142232b1a80567a40a404acd4687ddf25a6b0444
Jonas Biegert <jonas.biegert@gmx.de>
2018-12-05 17:38:54 +0100

    char repr., x = q,a y = 1 or 0

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -1,19 +1,21 @@
 import json
 import re
 import numpy as np
+import keras
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, SimpleRNN, TimeDistributed, GRU, LSTM
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Embedding, SimpleRNN, TimeDistributed, GRU, LSTM, Concatenate, Input
 from sklearn.metrics import accuracy_score
 
 
 def preprocess(filepath):
     strings = []
-    answers = []
+    available_choices = []
+    is_correct_answer = []
 
     vocab = {"unknown": 1}
-    wordInt = 2
+    charInt = 2
     with open(filepath) as json_file:
         data = json.load(json_file)
 
@@ -23,47 +25,57 @@ def preprocess(filepath):
             answer_key = item['answer']
             answer = item['choices'][answer_key]
 
-            if not re.match(r'^(([0-9]*)|(([0-9]*)\.([0-9]*)))$', answer):
+            if not re.match(r'^((-*)([0-9]*)|((-*)([0-9]*)\.([0-9]*)))$', answer):
                 continue
 
-            strings.append(item['question'])
-            answers.append(answer)
+            for choice in item['choices']:
+                strings.append(item['question'])
+                if re.match(r'^((-*)([0-9]*)|(([0-9]*)\.([0-9]*)))$', item['choices'][choice]):
+                    available_choices.append(item['choices'][choice])
+                else:
+                    available_choices.append(np.random.randint(0, 100))
+                if item['choices'][choice] == answer:
+                    is_correct_answer.append(1)
+                else:
+                    is_correct_answer.append(0)
 
-    training_strings, test_strings, training_answers, test_answers = [], [], [], []
+
+    training_strings, test_strings, train_choices, test_choices, train_y, test_y = [], [], [], [], [], []
 
     for i in range(0, len(strings)):
         if i < 4 / 5 * len(strings):
             training_strings.append(strings[i])
-            training_answers.append(answers[i])
+            train_y.append(is_correct_answer[i])
+            train_choices.append(available_choices[i])
         else:
             test_strings.append(strings[i])
-            test_answers.append(answers[i])
+            test_y.append(is_correct_answer[i])
+            test_choices.append(available_choices[i])
 
-    training_vectors = []
-    test_vectors = []
+
+    train_x = []
+    test_x = []
 
     for item in training_strings:
-        words = item.split(" ")
         currentRow = []
-        for item2 in words:
-            if item2 not in vocab.keys():
-                vocab[item2] = wordInt
-                wordInt += 1
-            currentRow.append(vocab[item2])
-        training_vectors.append(currentRow)
+        for char in item:
+            if char not in vocab.keys():
+                vocab[char] = charInt
+                charInt += 1
+            currentRow.append(vocab[char])
+        train_x.append(currentRow)
 
     for item in test_strings:
-        words = item.split(" ")
         currentRow = []
-        for item2 in words:
-            if item2 not in vocab.keys():
-                currentRow.append(vocab['unknown'])
+        for char in item:
+            if char not in vocab.keys():
+                currentRow.append(vocab["unknown"])
             else:
-                currentRow.append(vocab[item2])
-        test_vectors.append(currentRow)
+                currentRow.append(vocab[char])
+        test_x.append(currentRow)
 
 
-    return training_vectors, np.asarray(training_answers), test_vectors, np.asarray(test_answers), vocab
+    return train_x, np.asarray(train_y), np.asarray(train_choices), test_x, np.asarray(test_y), np.asarray(test_choices), vocab
 
 
 def to_onehot(array):
@@ -76,48 +88,60 @@ def to_onehot(array):
     return np.asarray(array_onehot)
 
 if __name__ == "__main__":
-    training_vectors_unpadded, training_answers, test_vectors_unpadded, test_answers, vocab = preprocess('../data/sat.train.json')
+    train_x_unpadded, train_y, train_choices, test_x_unpadded, test_y, test_choices, vocab = preprocess('../data/sat.train.json')
 
-    training_vectors = pad_sequences(training_vectors_unpadded)
-    test_vectors = pad_sequences(test_vectors_unpadded)
-    test_vectors = test_vectors[:, 6:]
+    train_x = pad_sequences(train_x_unpadded)
+    test_x = pad_sequences(test_x_unpadded, maxlen= len(train_x[0]))
+
+    for item in train_choices:
+        print(item)
 
     #training_vectors = to_onehot(training_vectors)
     #test_vectors = to_onehot(test_vectors)
-    """
-    rnn = Sequential((Embedding(len(vocab), 32, mask_zero=True, input_length= len(training_vectors[0])),
-                      Dropout(0.5),
-                      SimpleRNN(64, return_sequences=True),
-                      Dropout(0.5),
-                      TimeDistributed(Dense(len(vocab), activation='softmax'))
-                      ))
 
-    rnn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    rnn.fit(training_vectors, training_answers, epochs = 5, batch_size=32)
+    questions_input = Input(shape=(len(train_x[0]),))
+    x = Embedding(input_dim=len(vocab) + 1, output_dim=64, input_length=len(train_x[0]))(questions_input)
+    #questions_dense = Dense(2,)(questions_input)
+    choices_input = Input(shape=(1,))
+   # questions_dense = Dense(2, )(choices_input)
+    x = keras.layers.concatenate([x, choices_input], axis=1)
+    x = GRU(64)(x)
 
-    predictions = rnn.predict(test_vectors)
-    """
-    rnn = Sequential()
-    rnn.add(Embedding(input_dim=len(vocab) + 1, output_dim=64, input_length=len(training_vectors[0])))
-    rnn.add(Dropout(0.5))
-    rnn.add(SimpleRNN(units=64))
-    rnn.add(Dense(1, activation="linear"))
-    rnn.compile(loss="mean_absolute_error", optimizer="adam", metrics=['accuracy'])
-    rnn.fit(training_vectors, training_answers, epochs=40, batch_size=32)
+    #x = keras.layers.concatenate([x, choices_input], axis=1)
+    output = Dense(1, activation="softmax")(x)
 
-    predictions_raw = rnn.predict(test_vectors)
+    model = Model(inputs=[questions_input, choices_input], outputs=output)
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+    model.fit([train_x, train_choices], train_y, epochs=20, batch_size=32)
+
+    predictions = model.predict([test_x, test_choices])
 
     matches = 0
 
-    predictions = []
-    for entry in predictions_raw:
-        for entry2 in entry:
-            predictions.append(int(round(entry2)))
-
-    print(test_answers)
+    print(test_y)
     print(predictions)
-    for prediction, answer in zip(predictions, test_answers):
-        if str(prediction) == answer:
+    for prediction, answer in zip(predictions, test_y):
+        if prediction == answer:
             matches += 1
 
     print(matches)
+    #questions_input.add(Embedding(input_dim=len(vocab) + 1, output_dim=64, input_length=len(train_x[0])))(train_x)
+    """
+    rnn.add(Dropout(0.5))
+    rnn.add(SimpleRNN(units=64))
+    rnn.add(Dense(2, activation="softmax"))
+    rnn.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+    rnn.fit(train_x, train_y, epochs=20, batch_size=32)
+
+    predictions = rnn.predict(test_x)
+
+    matches = 0
+    
+    print(test_y)
+    print(predictions)
+    for prediction, answer in zip(predictions, test_y):
+        if prediction == answer:
+            matches += 1
+
+    print(matches)
+"""
