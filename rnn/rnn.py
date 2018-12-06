fb8d84e366e29bbf5fc66f00ff3561a067c73e9e
Jonas Biegert <jonas.biegert@gmx.de>
2018-12-06 19:03:16 +0100

    functional, but ineffective

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -6,7 +6,6 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Embedding, SimpleRNN, TimeDistributed, GRU, LSTM, Concatenate, Input
-from sklearn.metrics import accuracy_score
 
 
 def preprocess(filepath):
@@ -14,7 +13,7 @@ def preprocess(filepath):
     available_choices = []
     is_correct_answer = []
 
-    vocab = {"unknown": 1}
+    vocab = {'unknown': 1}
     charInt = 2
     with open(filepath) as json_file:
         data = json.load(json_file)
@@ -43,7 +42,7 @@ def preprocess(filepath):
     training_strings, test_strings, train_choices, test_choices, train_y, test_y = [], [], [], [], [], []
 
     for i in range(0, len(strings)):
-        if i < 4 / 5 * len(strings):
+        if i < 4 / 5 * len(strings) + 3:
             training_strings.append(strings[i])
             train_y.append(is_correct_answer[i])
             train_choices.append(available_choices[i])
@@ -69,13 +68,14 @@ def preprocess(filepath):
         currentRow = []
         for char in item:
             if char not in vocab.keys():
-                currentRow.append(vocab["unknown"])
+                currentRow.append(vocab['unknown'])
             else:
                 currentRow.append(vocab[char])
         test_x.append(currentRow)
 
 
-    return train_x, np.asarray(train_y), np.asarray(train_choices), test_x, np.asarray(test_y), np.asarray(test_choices), vocab
+    return train_x, np.asarray(train_y), np.asarray(train_choices), test_x, np.asarray(test_y), \
+           np.asarray(test_choices), vocab, len(data)
 
 
 def to_onehot(array):
@@ -87,61 +87,69 @@ def to_onehot(array):
 
     return np.asarray(array_onehot)
 
-if __name__ == "__main__":
-    train_x_unpadded, train_y, train_choices, test_x_unpadded, test_y, test_choices, vocab = preprocess('../data/sat.train.json')
+if __name__ == '__main__':
+    train_x_unpadded, train_y, train_choices, test_x_unpadded, test_y, test_choices, vocab,\
+        train_length = preprocess('../data/sat.train.json')
 
     train_x = pad_sequences(train_x_unpadded)
     test_x = pad_sequences(test_x_unpadded, maxlen= len(train_x[0]))
 
-    for item in train_choices:
-        print(item)
-
-    #training_vectors = to_onehot(training_vectors)
-    #test_vectors = to_onehot(test_vectors)
-
     questions_input = Input(shape=(len(train_x[0]),))
-    x = Embedding(input_dim=len(vocab) + 1, output_dim=64, input_length=len(train_x[0]))(questions_input)
-    #questions_dense = Dense(2,)(questions_input)
     choices_input = Input(shape=(1,))
-   # questions_dense = Dense(2, )(choices_input)
-    x = keras.layers.concatenate([x, choices_input], axis=1)
-    x = GRU(64)(x)
 
-    #x = keras.layers.concatenate([x, choices_input], axis=1)
-    output = Dense(1, activation="softmax")(x)
+    x = Embedding(input_dim=len(vocab) + 1, output_dim=64, input_length=len(train_x[0]))(questions_input)
+    x = Dropout(0.5)(x)
+    x = GRU(64)(x)
+    x = Dropout(0.5)(x)
+    x = keras.layers.concatenate([x, choices_input], axis=1)
+
+    output = Dense(1, activation='sigmoid')(x)
 
     model = Model(inputs=[questions_input, choices_input], outputs=output)
-    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.fit([train_x, train_choices], train_y, epochs=20, batch_size=32)
 
-    predictions = model.predict([test_x, test_choices])
+    test_predictions = model.predict([test_x, test_choices])
+    test_matches = 0
 
-    matches = 0
+    current_question = test_x[0]
+    options = []
+    for prediction, question in zip(test_predictions, test_x):
+        if np.array_equal(question, current_question):
+            options.append(float(prediction))
+        else:
+            answer = (max(options))
+            if test_y[test_predictions.tolist().index([answer])] == 1:
+                test_matches += 1
+            options = []
+            current_question = question
+            options.append(prediction)
+    answer = (max(options))
+    if test_y[test_predictions.tolist().index([answer])] == 1:
+        test_matches += 1
 
-    print(test_y)
-    print(predictions)
-    for prediction, answer in zip(predictions, test_y):
-        if prediction == answer:
-            matches += 1
+    print('Matches for the testset:', test_matches)
 
-    print(matches)
-    #questions_input.add(Embedding(input_dim=len(vocab) + 1, output_dim=64, input_length=len(train_x[0])))(train_x)
-    """
-    rnn.add(Dropout(0.5))
-    rnn.add(SimpleRNN(units=64))
-    rnn.add(Dense(2, activation="softmax"))
-    rnn.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
-    rnn.fit(train_x, train_y, epochs=20, batch_size=32)
+    train_predictions = model.predict([train_x, train_choices])
+    train_matches = 0
 
-    predictions = rnn.predict(test_x)
+    current_question = train_x[0]
+    options = []
+    for prediction, question in zip(train_predictions, train_x):
+        if np.array_equal(question, current_question):
+            options.append(float(prediction))
+        else:
+            answer = (max(options))
+            if train_y[train_predictions.tolist().index([answer])] == 1:
+                train_matches += 1
+            options = []
+            current_question = question
+            options.append(prediction)
+    answer = (max(options))
+    if train_y[train_predictions.tolist().index([answer])] == 1:
+        train_matches += 1
 
-    matches = 0
-    
-    print(test_y)
-    print(predictions)
-    for prediction, answer in zip(predictions, test_y):
-        if prediction == answer:
-            matches += 1
+    print('Matches for the testset:', train_matches)
 
-    print(matches)
-"""
+    #with a sigmoid activation function, the network actually produces an outputs other than 1,
+    #but the outputs tend to be very close so every prediction is still pretty much the same, just not 1
