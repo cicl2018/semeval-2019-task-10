38c0e0fe85ef6529fca658439afa72f91e77422a
MegamindHenry <hakunamatatahenry@gmail.com>
2018-12-13 16:50:39 +0100

    add json with glf questions

--- data_analysis/glf_questions.py
+++ data_analysis/glf_questions.py
@@ -1,19 +1,11 @@
 import json
-from pprint import pprint
-import re
 
 with open('sat.train.json') as fq:
     datafq = json.load(fq)
 with open('gold_logical_form_train.json') as fglf:
 	datafglf = json.load(fglf)
 
-# print(type(datafglf))
-# print(type(datafq))
-glf_id_list = []
 l_questions_with_glf = []
-
-for x in datafglf:
-	glf_id_list.append(x['id'])
 
 for x in datafq:
 	for y in datafglf:
@@ -21,14 +13,7 @@ for x in datafq:
 			x['logicalForm'] = y['logicalForm']
 			l_questions_with_glf.append(x)
 
-
-# print(len(glf_id_list))
-# q_with_glf_output = json.dump(l_questions_with_glf, indent=4)
-
 q_with_glf_output = json.dumps(l_questions_with_glf, indent = 4)
 
-# print(len(l_questions_with_glf))
-print(q_with_glf_output)
-# print(datafglf)
-
-
+with open('questions_with_glf.json', 'w+') as f:
+	f.write(q_with_glf_output)
