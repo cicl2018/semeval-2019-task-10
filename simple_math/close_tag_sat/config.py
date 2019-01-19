4f6ef973fd416b6ab884a7b9a246e41e5cd87336
Xuefeng Luo <hakunamatatahenry@gmail.com>
2019-01-19 13:38:00 +0100

    update config

--- simple_math/close_tag_sat/config.py
+++ simple_math/close_tag_sat/config.py
@@ -6,22 +6,25 @@ global BATCH_SIZE
 BATCH_SIZE = 128
 # for length of a question 150, 350 or 650
 global MAX_LENGTH_Q
-MAX_LENGTH_Q = 150
+MAX_LENGTH_Q = 650
 # for make up data sise
 global TRAINING_SIZE
-TRAINING_SIZE = 10000
+TRAINING_SIZE = 100000
+DATA_SIZE = "600k"
 # for train iteration
 global TRAIN_ITER
-TRAIN_ITER = 2
+TRAIN_ITER = 40
 # for pre train iteration
 global PRE_TRAIN_ITER
-PRE_TRAIN_ITER = 2
+PRE_TRAIN_ITER = 300
 
 global PRE_TRAIN_MODEL
-PRE_TRAIN_MODEL = "pre_trained_model_20_70k_150.h5"
+PRE_TRAIN_MODEL = "pre_trained_model_" + str(PRE_TRAIN_ITER) + "_" + str(DATA_SIZE)\
+                  + "_" + str(MAX_LENGTH_Q) + ".h5"
 
 global TRAINED_MODEL
-TRAINED_MODEL = "well_trained_model_20_70k_150_20.h5"
+TRAINED_MODEL = "well_trained_model_" + str(PRE_TRAIN_ITER) + "_" + str(DATA_SIZE)\
+                + "_" + str(MAX_LENGTH_Q) + "_" + str(TRAIN_ITER) + ".h5"
 
 
 
