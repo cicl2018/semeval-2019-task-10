d63bfee382d78f6aba805f32fb07d33ce03bfa4e
Xuefeng Luo <hakunamatatahenry@gmail.com>
2019-01-19 13:42:37 +0100

    quickfix

--- simple_math/close_tag_sat/config.py
+++ simple_math/close_tag_sat/config.py
@@ -4,19 +4,23 @@ from encode import InputChars, OutputChars
 # for training on a GPU change it to 1024
 global BATCH_SIZE
 BATCH_SIZE = 128
+
 # for length of a question 150, 350 or 650
 global MAX_LENGTH_Q
 MAX_LENGTH_Q = 650
+
 # for make up data sise
 global TRAINING_SIZE
 TRAINING_SIZE = 100000
 DATA_SIZE = "600k"
-# for train iteration
-global TRAIN_ITER
-TRAIN_ITER = 40
+
 # for pre train iteration
 global PRE_TRAIN_ITER
-PRE_TRAIN_ITER = 300
+PRE_TRAIN_ITER = 40
+
+# for train iteration
+global TRAIN_ITER
+TRAIN_ITER = 300
 
 global PRE_TRAIN_MODEL
 PRE_TRAIN_MODEL = "pre_trained_model_" + str(PRE_TRAIN_ITER) + "_" + str(DATA_SIZE)\
