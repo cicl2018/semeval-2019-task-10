253a0e5a5fada4f41c1d8b38519bf5310c66d1d3
AlinaBaranova <baranovaalinka@gmail.com>
2019-02-12 13:02:03 +0000

    Add a possibility to include reverse operations

--- tree_system/all_possible_trees.py
+++ tree_system/all_possible_trees.py
@@ -4,7 +4,7 @@ import json
 from itertools import combinations
 
 # returns all possible non-monotomic trees for an array of quantities
-def all_trees_for_question(arr):
+def all_trees_for_question(arr, ifReverse):
 	enum_trees = []
 
 	# generate all possible trees with the amount of quantities from 2 till the total number of quantities
@@ -16,18 +16,19 @@ def all_trees_for_question(arr):
 	# fill every tree with all possible combinations of operation signs
 	oper_trees = []
 	for enum_tree in enum_trees:
-		for oper_tree in operations_for_nodes(enum_tree):
+		for oper_tree in operations_for_nodes(enum_tree, ifReverse):
 			oper_trees.append(oper_tree)
 
 	# exclude non-monotomic trees
 	right_trees = []
 	for tree in oper_trees:
-		if check_monotomic(tree):
+		if check_monotomic(tree, ifReverse):
 			right_trees.append(tree)
 
 	return right_trees
 
-def all_trees_for_all_questions(filename):
+# returns a dictionary where key is question id, and value is all possible trees for quantities of this question
+def all_trees_for_all_questions(filename, ifReverse):
 	with open(filename) as file:
 		questions = json.load(file)
 	trees = {}
@@ -45,7 +46,7 @@ def all_trees_for_all_questions(filename):
 					new_q["value"] = q
 					arr_q.append(new_q)
 
-				all_trees = all_trees_for_question(arr_q)
+				all_trees = all_trees_for_question(arr_q, ifReverse)
 			except ValueError:
 				continue
 		trees[id] = all_trees
