16747c159aa09615a01d9572a3896b84e5a4051d
AlinaBaranova <baranovaalinka@gmail.com>
2019-02-13 15:01:26 +0000

    Reverse operations are possible

--- tree_system/building_trees.py
+++ tree_system/building_trees.py
@@ -26,21 +26,23 @@ def enum_unordered(labels):
 # print(len(all_trees))
 
 # assigns operations for tree nodes, provides all possible enumerations
-def operations_for_nodes(tree):
+def operations_for_nodes(tree, ifReverse):
 	# set of operations
-	# operations = ["+", "-", "-rev", "x", "/", "/rev"]
-	operations = ["+", "-", "x", "/"]
+	if ifReverse:
+		operations = ["+", "-", "-rev", "x", "/", "/rev"]
+	else:
+		operations = ["+", "-", "x", "/"]
 	# if node is "empty", choose an operation
 	if tree.data == None:
 		for operation in operations:
 			# if left child's value is None, choose operations to it
 			if tree.left.data == None:
 				# variants of left child (for every value of node)
-				for left_child in operations_for_nodes(tree.left):
+				for left_child in operations_for_nodes(tree.left, ifReverse):
 					# right child's value is None, choose operations to it
 					if tree.right.data == None:
 						# variants of right child (for every value of node and of left child)
-						for right_child in operations_for_nodes(tree.right):
+						for right_child in operations_for_nodes(tree.right, ifReverse):
 							# combine together operation for node, variant of left child and variant of right child
 							yield Node(operation, left_child, right_child)
 					# if right child doesn't exist or its value is not None, do not change right part of tree
@@ -50,7 +52,7 @@ def operations_for_nodes(tree):
 			# if right child's value is None (but left child's value is not None), choose operations for right child's value
 			elif tree.right.data == None:
 				# variants of right child (for every value of node)
-				for right_child in operations_for_nodes(tree.right):
+				for right_child in operations_for_nodes(tree.right, ifReverse):
 					# combine together operation for node, left child of tree and variant of right child
 					yield Node(operation, tree.left, right_child)
 			# if node is a leaf (both left child and right child have values different from None), do not change left or right part of tree
@@ -76,14 +78,13 @@ def operations_for_nodes(tree):
 # 	print(tree)
 
 # perform the operation in the node data on two of its leaves-children (quantities)
-def calculate_node(node):
+def calculate_node(node, ifReverse):
 	operation = node.data
 	left_data = node.left.data
 	right_data = node.right.data
 	if left_data != None and right_data != None:
 		if type(left_data) is dict:
 			left_data = left_data["value"]
-			# print(left_data)
 		if type(right_data) is dict:
 			right_data = right_data["value"]
 
@@ -91,39 +92,42 @@ def calculate_node(node):
 			return (left_data + right_data)
 		elif operation == "-":
 			return (left_data - right_data)
-		# elif operation == "-rev":
-		# 	return (node.right.data - node.left.data)
 		elif operation == "x":
 			return (left_data * right_data)
-		elif operation == "/" and node.right.data != 0.0:
+		elif operation == "/" and right_data != 0.0:
 			return (left_data / right_data)
-		# elif operation == "/rev" and node.left.data != 0.0:
-		# 	return (node.right.data / node.left.data)
+		if ifReverse:
+			if operation == "-rev":
+				return (right_data - left_data)
+			elif operation == "/rev" and left_data != 0.0:
+				return (right_data / left_data)
 	return None
 
 # calculate the outcome of tree based on nodes filled with operations
-def calculate_tree(tree):
-	# operations = ["+", "-", "-rev", "x", "/", "/rev"]
-	operations = ["+", "-", "x", "/"]
+def calculate_tree(tree, ifReverse):
+	if ifReverse:
+		operations = ["+", "-", "-rev", "x", "/", "/rev"]
+	else:
+		operations = ["+", "-", "x", "/"]
 
 	# if children of node are leaves, use calculate_node to perform the operation on them
 	if (tree.left.data not in operations) and (tree.right.data not in operations):
-		return calculate_node(tree)
+		return calculate_node(tree, ifReverse)
 
 	# if a child or both children of node aren't leaves, calculate left and right part separately, 
 	# and use the results to perform the operation in the node
 	else:
 		if tree.left.data in operations:
-			left_component = calculate_tree(tree.left)
+			left_component = calculate_tree(tree.left, ifReverse)
 		else:
 			left_component = tree.left.data
 
 		if tree.right.data in operations:
-			right_component = calculate_tree(tree.right)
+			right_component = calculate_tree(tree.right, ifReverse)
 		else:
 			right_component = tree.right.data
 
-		return calculate_node(Node(tree.data, Node(left_component), Node(right_component)))
+		return calculate_node(Node(tree.data, Node(left_component), Node(right_component)), ifReverse)
 
 # n1 = Node(4)
 # n2 = Node(8)
@@ -134,9 +138,12 @@ def calculate_tree(tree):
 
 # print(calculate_tree(n6))
 
-def check_monotomic(tree):
-	operations = ["+", "-", "x", "/"]
-	# operations = ["+", "-", "-rev", "x", "/", "/rev"]
+# check if tree is monotomic based on two constraints
+def check_monotomic(tree, ifReverse):
+	if ifReverse:
+		operations = ["+", "-", "-rev", "x", "/", "/rev"]
+	else:
+		operations = ["+", "-", "x", "/"]
 
 	# if node is leaf node (quantity)
 	if tree.data not in operations:
@@ -145,28 +152,29 @@ def check_monotomic(tree):
 	if tree.data == "+":
 		if tree.left.data == "-" or tree.right.data == "-":
 			return False
-		# if tree.left.data == "-rev" or tree.right.data == "-rev":
-		# 	return False
+		if ifReverse:
+			if tree.left.data == "-rev" or tree.right.data == "-rev":
+				return False
 	# checking constraint #2: If a multiplication node is connected to a division node, then the division node is the parent
 	if tree.data == "x":
 		if tree.left.data == "/" or tree.right.data == "/":
 			return False
-		# if tree.left.data == "/rev" or tree.right.data == "/rev":
-		# 	return False
+		if ifReverse:
+			if tree.left.data == "/rev" or tree.right.data == "/rev":
+				return False
 
 	# if check gets to this point, recursively apply the function to left and right children of the node
-	left_check = check_monotomic(tree.left)
-	right_check = check_monotomic(tree.right)
+	left_check = check_monotomic(tree.left, ifReverse)
+	right_check = check_monotomic(tree.right, ifReverse)
 	return (left_check and right_check)
 
-#n1 = Node(4)
-#n2 = Node(8)
-#n3 = Node(10)
-#n4 = Node(5)
+# n1 = Node(4)
+# n2 = Node(8)
+# n3 = Node(10)
+# n4 = Node(5)
 
-#n5 = Node("/", n1, n2)
-#n6 = Node("x", n3, n4)
-#tree = Node("+", n5, n6)
+# n5 = Node("/", n1, n2)
+# n6 = Node("x", n3, n4)
+# tree = Node("/", n5, n6)
 
-#print(calculate_tree(tree))
 # print(check_monotomic(tree))
